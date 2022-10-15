6b2063d605ce13478f23a35dd8ca1e423d7838ff
edupq1999 <nivel.17@hotmail.com>
2022-10-15 15:31:07 -0500

    Se agregaron estilos y el boton de dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
@@ -7,8 +8,21 @@ import ProtectedRoutes from './pages/ProtectedRoutes'
 
 function App() {
 
+  const [color, setColor] = useState('light')
+
+  const toggle = () => {
+    color === 'light'
+    ?
+    setColor('dark')
+    :
+    setColor('light')
+  }
+
   return (
-    <div className="App">
+    <div className='App' id={color}>
+      <div className='App_tittle'>
+        <img src="https://i.pinimg.com/originals/bd/cd/20/bdcd20f5411ee5785889542d303ad4cb.png" alt="pokedex_img" />
+      </div>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route element={<ProtectedRoutes/>} />
@@ -16,6 +30,21 @@ function App() {
           <Route path='/pokedex/:id' element={<PokemonId/>} />
         <Route/>
       </Routes>
+      <div className='handleColor' onClick={toggle}>
+        {
+          color === 'light'
+          ?
+          '🌑'
+          :
+          '🌞'
+        }
+      </div>
+      <footer className='general_footer'>
+        <div className='general_footer_middle'>
+          <div className='general_footer_middle_circle'></div>
+          <div className='general_footer_middle_circle'></div>
+        </div>
+      </footer>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import FormHome from '../components/home/FormHome'
+import './styles/home.css'
 
 const Home = () => {
   return (
-    <div>
-      <h1 className='home_tittle'>Pokedex</h1>
+    <div className='home'>
+      <img src="https://images.wikidexcdn.net/mwuploads/esssbwiki/thumb/c/c0/latest/20210725170725/Entrenador_Pok%C3%A9mon_RojoFuego_y_VerdeHoja.png/1200px-Entrenador_Pok%C3%A9mon_RojoFuego_y_VerdeHoja.png" alt="trainer_img" />
       <h2 className='home_greeting'>Hi trainer!</h2>
       <p className='home_text'>Give me your name to start</p>
       <FormHome />
